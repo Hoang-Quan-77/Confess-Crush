570c07add425f9f8d12f3882ac0d0f7083f0f0e8
Hoang-Quan-77 <89618478+Hoang-Quan-77@users.noreply.github.com>
2021-11-19 14:39:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
- titleWeb: "Gửi Hoàng Quân",
-    introTitle: 'Hoàng Quân', 
-    introDesc: `Trái đất vốn lạ thường
+    titleWeb: "Gửi Huỳnh Thị Mỹ Trinh",
+    introTitle: 'Tên Huỳnh Thị Mỹ Trinh',
+    introDesc: `Trái đất  vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã yêu từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say từ lúc thấy nụ cười ấy 🥰 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/hoangquan.060502/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100040220841900' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
