904c37190459eb18a2bf509e6221d3c2b1eb1d42
Hoang-Quan-77 <89618478+Hoang-Quan-77@users.noreply.github.com>
2021-11-19 14:22:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi Đỗ Hồng Quân",
-    introTitle: 'Đỗ Hồng Quân',
+ titleWeb: "Gửi Hoàng Quân",
+    introTitle: 'Hoàng Quân', 
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
